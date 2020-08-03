3752ec8e31873d0cc1a410d997bc6efee72c245c
james <jamesnofs@quickenloans.com>
2020-08-03 18:22:11 -0400

    lab 4.3 Prime numbers - Testing with Xunit
    Signed-off-by: james <jamesnofs@quickenloans.com>

---
+++ PrimeNumbersXunit.cs
@@ -0,0 +1,23 @@
+﻿using lab4._3_Prime_Numbers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace lab4._3_Prime_Numbers_Xunit
+{
+    public class PrimeNumbersXunit
+    {
+        [Theory]
+        [InlineData(2, true)]
+        [InlineData(3, true)]
+        [InlineData(4, false)]
+        [InlineData(7, true)]
+        [InlineData(12, false)]
+        public void Test1(int number, bool expected)
+        {
+            bool res = PrimeNumbers.getPrime(number);
+            Assert.Equal(expected, res);
+        }
+    }
+}
